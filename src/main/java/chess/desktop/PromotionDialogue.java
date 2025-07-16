499405f42d11d6bccbbebca69dc428e31edc3dba
Danylo Shevchuk <danny.sommar@gmail.com>
2025-07-17 00:51:19 +0100

    made it prettier, also red cancel button now works

--- src/main/java/chess/desktop/PromotionDialogue.java
+++ src/main/java/chess/desktop/PromotionDialogue.java
@@ -6,6 +6,7 @@ import chess.core.piece.Horse;
 import chess.core.piece.Piece;
 import chess.core.piece.Queen;
 import chess.core.piece.Rook;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
@@ -19,25 +20,44 @@ public class PromotionDialogue extends Dialog<Piece>
         setTitle("Pawn Promotion");
         setHeaderText("choose what to prome to");
 
+        // mainly adding this button because I don't want the others to appear blu on macOS, nor chnge ButtonData enum
+        ButtonType randomButton = new ButtonType("Feeling Lucky", ButtonData.OK_DONE);
+        
         ButtonType queenButton = new ButtonType("Queen", ButtonData.OK_DONE);
         ButtonType rookButton = new ButtonType("Rook", ButtonData.OK_DONE);
         ButtonType bishopButton = new ButtonType("Bishop", ButtonData.OK_DONE);
         ButtonType horseButton = new ButtonType("Horse", ButtonData.OK_DONE);
 
-        getDialogPane().getButtonTypes().addAll(queenButton, rookButton, bishopButton, horseButton);
+        getDialogPane().getButtonTypes().addAll(randomButton, queenButton, rookButton, bishopButton, horseButton);
 
-        getDialogPane().setContent(new HBox(10,
+        // the red close button without having to add a cancel button (I hate frontend why)
+        getDialogPane().getScene().getWindow().setOnCloseRequest(e -> {setResult(null);});
+
+
+        // just display the pieces
+        HBox imageOptions = new HBox(60);
+        imageOptions.setPadding(new Insets(0, 0, 0, 160));
+
+        imageOptions.getChildren().addAll(
             createPromotionPicture(new Queen(pieceColour)),
             createPromotionPicture(new Rook(pieceColour)),
             createPromotionPicture(new Bishop(pieceColour)),
             createPromotionPicture(new Horse(pieceColour))
-        ));
+        );
+        getDialogPane().setContent(imageOptions);
 
+        // button logic
         setResultConverter(buttonType -> {
             if (buttonType == queenButton) return new Queen(pieceColour);
             if (buttonType == rookButton) return new Rook(pieceColour);
             if (buttonType == bishopButton) return new Bishop(pieceColour);
             if (buttonType == horseButton) return new Horse(pieceColour);
+
+            if (buttonType == randomButton)
+            {
+                Piece[] possiblePieces = {new Queen(pieceColour), new Rook(pieceColour), new Bishop(pieceColour), new Horse(pieceColour)};
+                return possiblePieces[(int) ((Math.random() * (4 - 0)) + 0)];
+            }
             return null;
         });
 
